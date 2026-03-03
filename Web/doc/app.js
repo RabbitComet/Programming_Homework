914093924b8de651c84734e0fa60090614285361
rabbitcomet <sakolol13@gmail.com>
2026-03-03 22:30:45 +0400

    history, onclick, document, querySelector

---
+++ Web/doc/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 8080;
+
+app.get(["/","/about","/contact"], (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(port, function () {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
